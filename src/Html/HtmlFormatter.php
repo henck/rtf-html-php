cba48500ae03b9213dcad4a2e50d3a1f9e3efd1c
mhtamala <32551786+mhtamala@users.noreply.github.com>
2019-09-20 12:41:19 +0100

    fix bug removing last 3 characters from document
    see Pull #62 (closed)

--- src/Html/HtmlFormatter.php
+++ src/Html/HtmlFormatter.php
@@ -46,11 +46,8 @@ class HtmlFormatter
     // Begin format
     $this->ProcessGroup($document->root);
     // Remove any final unclosed 'p' tag and return result:
-    if (substr($this->output, -3) == "<p>") {
-      return substr($this->output, 0, -3);
-    } else {
-      return $this->output;
-    }
+    return $this->openedTags['p'] ? substr($this->output, 0, -3) : $this->output;
+    
   }
 
   protected function LoadFont(\RtfHtmlPhp\Group $fontGroup) {
